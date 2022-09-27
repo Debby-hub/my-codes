b3d54d9b2f325ac8fe77d1d4ab7e2fbded77fc40
Debby-hub <114514927+Debby-hub@users.noreply.github.com>
2022-09-27 07:41:36 -1000

    Add files via upload

---
+++ debby stalker.js
@@ -0,0 +1,12 @@
+var firstname = prompt("what is your first name");
+var middlename = prompt("what is your middle name");
+var lastname = prompt("what is your last name");
+var age = prompt("what is your age");
+var level = prompt("what level are you?")
+console.log("hello " + firstname + " " + middlename + " " + lastname);
+console.log("you are " + age + " years old");
+console.log("you are in " + level + " in school");
+function multipy(x, y){
+var product=5*100;
+alert(product + " "+ "is also a number of 5");
+}
